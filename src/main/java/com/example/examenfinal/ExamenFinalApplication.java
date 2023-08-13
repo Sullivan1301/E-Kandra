f58c7952cbbdbdf8dc9c311f0c78896c67731ade
Sullivan13 <jorosullivan13@gmail.com>
2023-08-13 17:23:08 +0300

    initialize repository

--- src/main/java/com/example/examenfinal/ExamenFinalApplication.java
+++ src/main/java/com/example/examenfinal/ExamenFinalApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExamenFinalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExamenFinalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenFinalApplication.class, args);
+    }
 
 }
