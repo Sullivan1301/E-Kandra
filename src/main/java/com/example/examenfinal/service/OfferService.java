7566f35ed2f09449fa62a91d0e70c2703bf2bdda
Sullivan13 <jorosullivan13@gmail.com>
2023-08-24 15:48:33 +0300

    create OfferService class

--- src/main/java/com/example/examenfinal/service/OfferService.java
+++ src/main/java/com/example/examenfinal/service/OfferService.java
@@ -2,21 +2,37 @@ package com.example.examenfinal.service;
 
 import java.sql.SQLException;
 import java.util.List;
+
+import com.example.examenfinal.repository.OfferDAOInterface;
 import org.springframework.stereotype.Service;
 import com.example.examenfinal.entity.Offer;
 import com.example.examenfinal.repository.OfferDAO;
 
 @Service
 public class OfferService {
-    private OfferDAO dao;
+    private OfferDAOInterface dao;
 
-    public OfferService(OfferDAO dao) {
+    public OfferService(OfferDAOInterface dao) {
         this.dao = dao;
+    }
+
+    public Offer createOffer(Offer offer){
+        return dao.insert(offer);
     }
 
     public List<Offer> getAllOffers() throws SQLException {
         return dao.getAll();
     }
 
-    // Implémentez les autres méthodes de service pour créer, mettre à jour, supprimer des offres
+    public Offer getOfferById(int id){
+        return dao.getById(id);
+    }
+
+    public void updateOffer(Offer offer){
+        dao.update(offer);
+    }
+
+    public void deleteOffer(int id){
+        dao.delete(id);
+    }
 }
